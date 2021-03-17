1f48adb269765e9ad24ac60f2567eb354c5d2004
Zezuz <mczanaty92@gmail.com>
2021-03-16 19:37:29 -0500

    Learning Classes

---
+++ BasicClassesExercise/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Make { get; set; }
+
+    }
+}
---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Mercedes = new Car();
+
+            Mercedes.Year = "2019";
+            Mercedes.Make = "C Class";
+            Mercedes.Model = "Mercedes";
+
+            Console.WriteLine($"The car is a {Mercedes.Year} {Mercedes.Model} {Mercedes.Make}. ");
+        }
+    }
+}
